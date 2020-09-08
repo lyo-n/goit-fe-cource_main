1dfbb686a1055f8f01e0077d6c0320d4ea5e1176
lyo-n <1y0mmmm@gmail.com>
2020-09-08 21:42:12 +0300

    module-08/1

--- goit-js/goit-js-hw-08-gallery/JS/main.js
+++ goit-js/goit-js-hw-08-gallery/JS/main.js
@@ -8,7 +8,7 @@ const openImg = document.querySelector('.lightbox__image');
 const imgMain = images.reduce((acc, item) => {
 item = `<li class = "gallery__item">
             <div class = "gallery__link">
-                <div class = "img__gallery">
+                <div class = "img__gallery" href="${item.original}">
                     <img class = "gallery__image" src="${item.original}" data-source = "${item.original}" alt="${item.description}"/>
                         <div class = "gallery__icon">
                         <span class = "material-icons"></span>
