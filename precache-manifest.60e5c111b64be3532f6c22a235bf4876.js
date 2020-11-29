500d3a5329856775934c77d5e10fb4497553b64b
lyo-n <1y0mmmm@gmail.com>
2020-11-29 02:40:37 +0200

    Updates

---
+++ precache-manifest.60e5c111b64be3532f6c22a235bf4876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67205a64d143050cc26f5dd6f27ef322",
+    "url": "/goit-react-hw01-components/index.html"
+  },
+  {
+    "revision": "c1e7009ea7822b8133e9",
+    "url": "/goit-react-hw01-components/static/css/main.0d86c32e.chunk.css"
+  },
+  {
+    "revision": "f0d6ad04a3463b13f9e4",
+    "url": "/goit-react-hw01-components/static/js/2.1af95d3e.chunk.js"
+  },
+  {
+    "revision": "c1e7009ea7822b8133e9",
+    "url": "/goit-react-hw01-components/static/js/main.742cbe55.chunk.js"
+  },
+  {
+    "revision": "e142c57f94e59952dc91",
+    "url": "/goit-react-hw01-components/static/js/runtime-main.339d4a69.js"
+  }
+]);
